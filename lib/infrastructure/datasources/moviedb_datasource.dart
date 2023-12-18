2bf6bf3c99ef93d8b1d54f92d230515d9d4874cc
osvaldo <osvaldo.guerra.c008@gmail.com>
2023-12-18 14:58:12 -0600

    impl datasource getNowPlaying

---
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -0,0 +1,25 @@
+import 'package:cinemapedia/config/constants/environmnet.dart';
+import 'package:cinemapedia/domain/datasources/movies_datasources.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
+import 'package:dio/dio.dart';
+
+class MovieDBDatasource extends MoviesDatasource {
+  final dio = Dio(BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {
+        'api_key': Environmnet.movieDBKey,
+        'language': 'es-MX'
+      }));
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await dio.get('/movie/now_playing');
+    return MovieDbResponse.fromJson(response.data)
+        .results
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+        .toList();
+  }
+}
