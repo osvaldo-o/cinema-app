e293be4137ba407d4e040ddf80718569ee11e38f
osvaldo <osvaldo.guerra.c008@gmail.com>
2023-12-20 12:30:02 -0600

    provider getNowPlating

---
+++ lib/presentation/provider/movies/movies_providers.dart
@@ -0,0 +1,22 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/provider/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final nowPlayingMoviesProvider =
+    StateNotifierProvider<MovieNotifier, List<Movie>>((ref) {
+  final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPlaying;
+  return MovieNotifier(fetchMoreMovies: fetchMoreMovies);
+});
+
+typedef MovieCallback = Future<List<Movie>> Function({int page});
+
+class MovieNotifier extends StateNotifier<List<Movie>> {
+  int currentPage = 0;
+  MovieCallback fetchMoreMovies;
+  MovieNotifier({required this.fetchMoreMovies}) : super([]);
+
+  Future loadNextPage() async {
+    currentPage++;
+    state = [...state, ...await fetchMoreMovies(page: currentPage)];
+  }
+}
---
+++ lib/presentation/provider/movies/movies_repository_provider.dart
@@ -0,0 +1,6 @@
+import 'package:cinemapedia/infrastructure/datasources/moviedb_datasource.dart';
+import 'package:cinemapedia/infrastructure/repositories/movie_repository_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final movieRepositoryProvider =
+    Provider((ref) => MovieRepositoryImpl(datasource: MovieDBDatasource()));
